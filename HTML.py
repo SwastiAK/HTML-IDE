08fedc3bce48189da6377aaa12619cf254c1e158
SwastiAK <85284257+SwastiAK@users.noreply.github.com>
2022-07-14 13:08:20 +0530

    Add files via upload

--- HTML.py
+++ HTML.py
@@ -8,15 +8,16 @@ Created on Tue Jul 12 17:55:21 2022
 from tkinter import *
 from PIL import ImageTk, Image
 from tkinter import messagebox
+from tkinter import filedialog
 
 root = Tk()
 root.minsize(650,650)
 root.maxsize(650,650)
 root.configure(background = "lightblue")
 
-open_img = ImageTk.PhotoImage(Image.open("C:/Users/Swasti/Desktop/Python Projects/ADV-C160-Student/open.png"))
-save_img = ImageTk.PhotoImage(Image.open("C:/Users/Swasti/Desktop/Python Projects/ADV-C160-Student/save.png"))
-exit_img = ImageTk.PhotoImage(Image.open("C:/Users/Swasti/Desktop/Python Projects/ADV-C160-Student/exit.jpg"))
+open_img = ImageTk.PhotoImage(Image.open("C:/Users/Swasti/Desktop/Python Projects/HTML IDE/open.png"))
+save_img = ImageTk.PhotoImage(Image.open("C:/Users/Swasti/Desktop/Python Projects/HTML IDE/save.png"))
+exit_img = ImageTk.PhotoImage(Image.open("C:/Users/Swasti/Desktop/Python Projects/HTML IDE/exit.jpg"))
 
 label_file_name = Label(root, text="File Name")
 label_file_name.place(relx=0.28,rely=0.03,anchor= CENTER)
@@ -27,4 +28,29 @@ input_file_name.place(relx=0.46,rely=0.03, anchor= CENTER)
 my_text= Text(root,height=35,width=80, bg = "pink", fg = "black")
 my_text.place(relx=0.5,rely=0.55,anchor= CENTER)
 
+name = ""
+def openFile():
+    global name
+    my_text.delete(1.0, END)
+    input_file_name.delete(0, END)
+    html_file = filedialog.askopenfilename(title = "Open HTML File", filetypes = (("HTML Files", "*.html"), ))
+    print(html_file)
+    name = os.path.basename(html_file)
+    formated_name = name.split('.')[0]
+    input_file_name.insert(END, formated_name)
+    root.title(formated_name)
+    html_file = open(name,'r')
+    paragraph = text_file.read()
+    my_text.insert(END, paragraph)
+    html_file.close()
+
+open_button=Button(root, image = open_img, text="Open File", command = openFile)
+open_button.place(relx=0.05,rely=0.03,anchor=CENTER)
+
+save_button=Button(root, image = save_img, text="Save File")
+save_button.place(relx=0.11,rely=0.03,anchor= CENTER)
+
+exit_button=Button(root, image = exit_img, text="Exit File")
+exit_button.place(relx=0.17,rely=0.03,anchor= CENTER)
+    
 root.mainloop()
