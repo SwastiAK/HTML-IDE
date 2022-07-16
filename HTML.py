f4c50fbfdbc159a673e857451c37561cb019354f
SwastiAK <85284257+SwastiAK@users.noreply.github.com>
2022-07-16 10:46:04 +0530

    Add files via upload

--- HTML.py
+++ HTML.py
@@ -9,6 +9,8 @@ from tkinter import *
 from PIL import ImageTk, Image
 from tkinter import messagebox
 from tkinter import filedialog
+import os
+import webbrowser
 
 root = Tk()
 root.minsize(650,650)
@@ -17,7 +19,7 @@ root.configure(background = "lightblue")
 
 open_img = ImageTk.PhotoImage(Image.open("C:/Users/Swasti/Desktop/Python Projects/HTML IDE/open.png"))
 save_img = ImageTk.PhotoImage(Image.open("C:/Users/Swasti/Desktop/Python Projects/HTML IDE/save.png"))
-exit_img = ImageTk.PhotoImage(Image.open("C:/Users/Swasti/Desktop/Python Projects/HTML IDE/exit.jpg"))
+play_img = ImageTk.PhotoImage(Image.open("C:/Users/Swasti/Desktop/Python Projects/HTML IDE/play.png"))
 
 label_file_name = Label(root, text="File Name")
 label_file_name.place(relx=0.28,rely=0.03,anchor= CENTER)
@@ -33,24 +35,38 @@ def openFile():
     global name
     my_text.delete(1.0, END)
     input_file_name.delete(0, END)
-    html_file = filedialog.askopenfilename(title = "Open HTML File", filetypes = (("HTML Files", "*.html"), ))
+    html_file = filedialog.askopenfilename(title = "Open html File", filetypes = (("html Files", "*.html"), ))
     print(html_file)
     name = os.path.basename(html_file)
     formated_name = name.split('.')[0]
     input_file_name.insert(END, formated_name)
     root.title(formated_name)
     html_file = open(name,'r')
-    paragraph = text_file.read()
+    paragraph = html_file.read()
     my_text.insert(END, paragraph)
     html_file.close()
+    
+def save():
+    input_name = input_file_name.get()
+    file = open(input_name + ".html", "w")
+    data = my_text.get("1.0", END)
+    print(data)
+    file.write(data)
+    input_file_name.delete(0, END)
+    my_text.delete(1.0, END)
+    messagebox.showinfo("Update", "Success! Data Saved")
+    
+def playWindow():
+    global name
+    webbrowser.open(name)    
 
 open_button=Button(root, image = open_img, text="Open File", command = openFile)
 open_button.place(relx=0.05,rely=0.03,anchor=CENTER)
 
-save_button=Button(root, image = save_img, text="Save File")
+save_button=Button(root, image = save_img, text="Save File", command = save)
 save_button.place(relx=0.11,rely=0.03,anchor= CENTER)
 
-exit_button=Button(root, image = exit_img, text="Exit File")
-exit_button.place(relx=0.17,rely=0.03,anchor= CENTER)
+play_button=Button(root, image = play_img, text="Play File", command = playWindow)
+play_button.place(relx=0.17,rely=0.03,anchor= CENTER)
     
 root.mainloop()
